e61723ac652ddecebbc7cd12f92c8157c8bd0883
PK <pedrinhokauan824@gmai.com>
2026-09-08 14:27:52 -0300

    test(integration): cobre fluxos PostgreSQL

--- src/Aegis.Application/DependencyInjection.cs
+++ src/Aegis.Application/DependencyInjection.cs
@@ -13,6 +13,7 @@ public static class DependencyInjection
         services.AddScoped<LogoutUseCase>();
         services.AddScoped<GetMeUseCase>();
         services.AddScoped<ChangePasswordUseCase>();
+        services.AddScoped<DeactivateUserUseCase>();
         return services;
     }
 }
